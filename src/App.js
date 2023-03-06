923a0bfae5a1af3d06eaf445f639e6ef8a60ae74
suganthsugi <suganthjayanthi@gmail.com>
2023-03-07 00:21:34 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import QRCode from 'qrcode.react';
 
 function App() {
+
+  const [id, setId] = useState(0);
+  const [canShow, setCanShow] = useState(false)
+  const [isFetching, setIsFetching] = useState(true);
+  const [qr, setQr] = useState(0);
+  const [showQr, setShowQr] = useState(true)
+
+  useEffect(() => {
+    let interval;
+    if (isFetching) {
+      interval = setInterval(() => {
+        fetch(`https://qr-n5bh.onrender.com/${id}`)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data.data);
+            if (data.data === true) {
+              setIsFetching(false);
+              setCanShow(true);
+            }
+          })
+          .catch(error => console.error(error))
+      }, 3000);
+    }
+    return () => clearInterval(interval);
+  }, [isFetching, id]);
+
+  const QRCodeGenerator = ({ url }) => {
+    return (
+      <QRCode
+        value={url}
+        size={256}
+        fgColor="#000000"
+        bgColor="#FFFFFF"
+        level="H"
+        renderAs="svg"
+      />
+    );
+  };
+
+  const handleClick = () => {
+    fetch('https://qr-n5bh.onrender.com/new')
+      .then(response => response.json())
+      .then(data => { console.log(data.id); setId(data.id);  setQr(QRCodeGenerator({url:`https://qr-n5bh.onrender.com/update/${data.id}`}))}, setShowQr(true))
+      .catch(error => console.error(error))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Return Order</h3>
+      <Button onClick={handleClick}>New Return Order</Button>
+      <br></br>
+      <br></br>
+      {showQr?qr:""}
+      <br></br>
+      <h1>{canShow ? "Successfully submitted" : ""}</h1>
     </div>
   );
 }
